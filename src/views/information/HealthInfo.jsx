f3c9bba7e7763da1b1eaff617f7c4a903bac21d0
Uranchimeg Jamsran <uranchimeg@mezorn.com>
2023-11-08 23:44:41 +0800

    kasjndf

--- src/views/information/HealthInfo.jsx
+++ src/views/information/HealthInfo.jsx
@@ -3,13 +3,13 @@ import { Steps } from 'antd';
 import { useState } from "react";
 import { BiChevronRight, BiChevronLeft } from 'react-icons/bi'
 
-const CustomInput = ({ label = '', ph = 'input', unit = '' }) => {
+const CustomInput = ({ label = '', ph = '', unit = '', isLong = false }) => {
     return (
         <div className='flex flex-col gap-2'>
-            <p className='text-[#00000061] text-[12px]' >{label}</p>
-            <div className="border-b border-[#0000006B]">
+            <p className='text-[#757575] text-[12px]' >{label}</p>
+            <div className="border-b border-[#0000006B] flex pb-1">
                 <input className='w-full outline-none' placeholder={ph} />
-                <p>{unit}</p>
+                <p className={`text-[#757575] text-[14px] ${isLong && 'w-[60px]'}`}>{unit}</p>
             </div>
         </div>
     );
@@ -64,8 +64,36 @@ const HealthInfo = () => {
 
 const FirstStep = () => {
     return (
-        <div>
+        <div className="flex flex-col gap-6">
             <CustomInput label="健診日" ph={"2023/08/17"} />
+            <div className="flex gap-6">
+                <CustomInput label="身長" unit="cm" />
+                <CustomInput label="体重" unit="kg" />
+            </div>
+            <div className="flex gap-6">
+                <CustomInput label="腹囲" unit="cm" />
+                <div className="w-1/2"></div>
+            </div>
+            <CustomInput label="既往歴" />
+
+            {/* TODO */}
+
+            <div className="flex gap-6">
+                <CustomInput label="採血時間（食後）" isLong unit="時間後" />
+                <CustomInput label="中性脂肪(トリグリセリド)" unit="mg/dl" />
+            </div>
+            <div className="flex gap-6">
+                <CustomInput label="HDLコレステロール" unit="mg/dl" />
+                <CustomInput label="LDLコレステロール" unit="mg/dl" />
+            </div>
+            <div className="flex gap-6">
+                <CustomInput label="GOT（AST）" unit="U/I" />
+                <CustomInput label="GPT（ALT）" unit="U/I" />
+            </div>
+            <div className="flex gap-6">
+                <CustomInput label="γ-GT(γ-GTP)" unit="U/I" />
+                <div className="w-1/2"></div>
+            </div>
         </div>
     )
 }
