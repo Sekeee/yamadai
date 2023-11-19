4e8f65f5ae37b6ca816b8793c8b28fc9ce69f7ef
setgel jigmedochir <setgel@mezorn.com>
2023-11-20 01:15:00 +0800

    setgel chjanges

--- src/views/information/HealthInfo.jsx
+++ src/views/information/HealthInfo.jsx
@@ -110,19 +110,39 @@ const FirstStep = ({ data, setData }) => {
                 ph={"2023/08/17"}
             />
             <div className="flex gap-6">
-                <CustomInput label="身長" unit="cm" />
-                <CustomInput label="体重" unit="kg" />
+                <CustomInput
+                    onChange={(e) => changeData(e.target.value, '身長')}
+                    value={data?.身長 || ''}
+                    label="身長"
+                    unit="cm" />
+                <CustomInput
+                    onChange={(e) => changeData(e.target.value, '体重')}
+                    value={data?.体重 || ''}
+                    label="体重"
+                    unit="kg" />
             </div>
             <div className="flex gap-6">
-                <CustomInput label="腹囲" unit="cm" />
+                <CustomInput
+                    onChange={(e) => changeData(e.target.value, '腹囲')}
+                    value={data?.腹囲 || ''}
+                    label="腹囲"
+                    unit="cm" />
                 <div className="w-1/2"></div>
             </div>
-            <CustomInput label="既往歴" />
+            <CustomInput
+                onChange={(e) => changeData(e.target.value, '既往歴')}
+                value={data?.既往歴 || ''}
+                label="既往歴" />
 
             {/* TODO */}
 
             <div className="flex gap-6">
-                <CustomInput label="採血時間（食後）" isLong unit="時間後" />
+                <CustomInput
+                    onChange={(e) => changeData(e.target.value, '採血時間')}
+                    value={data?.採血時間 || ''}
+                    label="採血時間（食後）"
+                    isLong unit="時間後"
+                />
                 <CustomInput label="中性脂肪(トリグリセリド)" unit="mg/dl" />
             </div>
             <div className="flex gap-6">
