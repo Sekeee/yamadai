5855e8427beb0ecc5ac8b4eb383fa766fe1f7891
Bagana <altanbagana@mezorn.com>
2023-11-20 00:35:33 +0800

    bagana login

--- src/components/common/BorderLabelInput.js
+++ src/components/common/BorderLabelInput.js
@@ -1,22 +1,45 @@
-const BorderLabelInput = ({ label = '', type = 'texts', onChange, value, underLabel = false, ph = '' }) => {
-	return (
-		<div className='relative'>
-			<label
-				className='border-label'
-				for='first'
-			>
-				{label}
-			</label>
-			<input
-				value={value}
-				onChange={onChange}
-				className='border-label-input outline-none w-full'
-				type={type}
-				placeholder={ph}
-			/>
-			{underLabel && <label className='text-[#00000099] font-[12px]'>{underLabel}</label>}
-		</div>
-	);
+import React, { useState } from 'react';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+
+const BorderLabelInput = ({ label = '', type = 'text', onChange, onKeyDown, value, underLabel = false, ph = '' }) => {
+  const isPasswordInput = type === 'password';
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className='relative'>
+      <label className='border-label' htmlFor='first'>
+        {label}
+      </label>
+      {isPasswordInput ? (
+        <div className='relative'>
+          <input
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown} // Handle onKeyDown event
+            type={showPassword ? 'text' : 'password'}
+            className='border-label-input outline-none w-full bg-none hover:bg-none pr-10'
+            placeholder={ph}
+          />
+          <div
+            className='absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer'
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <EyeTwoTone /> : <EyeInvisibleOutlined />}
+          </div>
+        </div>
+      ) : (
+        <input
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown} // Handle onKeyDown event
+          className='border-label-input outline-none w-full focus:border-primary'
+          type='text'
+          placeholder={ph}
+        />
+      )}
+      {underLabel && <label className='text-[#00000099] font-[12px]'>{underLabel}</label>}
+    </div>
+  );
 };
 
 export default BorderLabelInput;
