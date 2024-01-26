cc3820da195a9a26c16fc996f9c03d5e5ef9c3e1
setgel jigmedochir <setgel@mezorn.com>
2024-01-26 13:16:24 +0800

    changes

--- src/views/advice/Detail.jsx
+++ src/views/advice/Detail.jsx
@@ -59,7 +59,7 @@ const AdviceDetail = () => {
         <div className='relative !h-screen flex flex-col overflow-y-scroll'>
             <Header title='健康増進アプリ' setIsDrawerOpen={setIsDrawerOpen} />
             <div className='w-full overflow-auto h-full pb-6'>
-                <p className='text-black text-xl mt-4 mx-6'>sekeboy</p>
+
 
             </div>
 
